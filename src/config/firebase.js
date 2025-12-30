ed87a6d102f62542270f75c0be8110c0a41646d3
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-12-30 19:05:14 +0700

    refactor: remove commented-out service account code from firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,16 +3,6 @@ import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-// Lấy path tuyệt đối tới file service account
-// const serviceAccountPath = path.join(import.meta.dirname, "../../forgotpassword-83562-firebase-adminsdk.json");
-
-
-
-// Đọc + parse file JSON chứa private key
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccount, "utf8"));
-
-// Khởi tạo Firebase Admin bằng service account
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
