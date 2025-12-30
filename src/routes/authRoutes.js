469660ea973de33b99dc44a2ddf9ff6704eed6a6
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-12-30 16:28:55 +0700

    feat: add Google login functionality with Firebase authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,6 +10,7 @@ import {
   getUsers,
   forgotPassword,
   resetPassword,
+  googleLoginController
 } from "../controllers/authController.js";
 import { protect, authorize } from "../middleware/authMiddleware.js";
 
@@ -155,4 +156,5 @@ router.get("/getUsers", protect, authorize('admin'), getUsers);
  */
 router.post('/forgot-password', forgotPassword);
 router.put('/reset-password/:token', resetPassword);
+router.post('/google-login', googleLoginController);
 export default router;
