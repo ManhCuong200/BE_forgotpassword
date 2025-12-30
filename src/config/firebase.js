a78ff76c00a86fbb68807a098304b6c248634dfa
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-12-30 18:37:15 +0700

    refactor: remove Firebase service account file and update initialization to use environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,24 @@
 import admin from "firebase-admin";
 import fs from "fs";
-import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Lấy path tuyệt đối tới file service account
-const serviceAccountPath = path.join(import.meta.dirname, "../../forgotpassword-83562-firebase-adminsdk.json");
+// const serviceAccountPath = path.join(import.meta.dirname, "../../forgotpassword-83562-firebase-adminsdk.json");
+
+
 
 // Đọc + parse file JSON chứa private key
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccount, "utf8"));
 
 // Khởi tạo Firebase Admin bằng service account
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 export default admin;
